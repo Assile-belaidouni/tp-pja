1ad41f4700b5f77ef3152fceef5a9a3ee576e156
Assile-belaidouni <93595261+Assile-belaidouni@users.noreply.github.com>
2021-11-14 07:47:30 +0100

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.io.Serializable;
+
+    public class Employee implements Serializable {
+        public String name;
+        public String address;
+        public transient int SSN;
+        public int number;
+    }
+
+
+
+
+
+
+
+
+
+
+
---
+++ class3.java
@@ -0,0 +1,33 @@
+package com.company;
+import java.io.*;
+import java.util.Scanner;
+
+    public class Class4 {
+
+        public static void main(String[] args) throws IOException, ClassNotFoundException {
+            // TODO Auto-generated method stub
+
+
+            Employee [] Employees= null;
+            FileInputStream fileIn = new FileInputStream("C:\\Users\\ASUS\\IdeaProjects\\PJA\\emp.dat");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            Employees= (Employee []) in.readObject();
+            in.close();
+            fileIn.close();
+
+            String erre = null;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter The Employee Number");
+            int nbr= sc.nextInt();
+
+            for (int i = 0; i < Employees.length; i++) {
+                if (Employees[i].number == nbr) {
+                    System.out.println("The employe Imnformations: \n");
+                    System.out.println("Name : " + Employees[i].name + "\n" + Employees[i].address + "\n" + Employees[i].SSN + "\n" + Employees[i].number + "\n");
+                }
+            }
+
+        }
+
+    }
+
